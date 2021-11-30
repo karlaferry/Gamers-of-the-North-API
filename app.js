4a344a9c862f05f517dd9c3a6141b577ea4c7e1b
karlaferry <karlacarreon.1994@gmail.com>
2021-11-30 16:50:53 +0000

    inserted labels

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+// REQUIRES
 const express = require("express");
 const apiRouter = require("./routers/api.router");
 const { customErrorHandling, handle404 } = require("./errors/errors");
+
+// CREATE SERVER
 const app = express();
 app.use(express.json());
+
+// ROUTER
 app.use("/api", apiRouter);
 
+// ERROR HANDLING
 app.use(customErrorHandling);
 app.all("/*", handle404);
+
 module.exports = app;
