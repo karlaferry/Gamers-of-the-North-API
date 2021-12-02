9d3fd6bd4368256e104c8e40f280c0ea252520f4
karlaferry <karlacarreon.1994@gmail.com>
2021-12-02 12:19:18 +0000

    edited route

--- routers/api.router.js
+++ routers/api.router.js
@@ -2,7 +2,7 @@ const apiRouter = require("express").Router();
 const categoriesRouter = require("./categories.router");
 const reviewsRouter = require("./reviews.router");
 // apiRouter.route()
-apiRouter.route("/", (req, res, next) => {
+apiRouter.route("/").get((req, res, next) => {
   res.status(200).send({ msg: "Hello world!" });
 });
 apiRouter.use("/categories", categoriesRouter);
