b5bf28f6037eaa3165b4368a127c6a9618a5d391
karlaferry <karlacarreon.1994@gmail.com>
2021-12-02 16:13:42 +0000

    added 500 error handling

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // REQUIRES
 const express = require("express");
 const apiRouter = require("./routers/api.router");
-const { customErrorHandling, handle404 } = require("./errors/errors");
+const {
+  customErrorHandling,
+  handle404,
+  handle500,
+} = require("./errors/errors");
 
 // CREATE SERVER
 const app = express();
@@ -12,6 +16,7 @@ app.use("/api", apiRouter);
 
 // ERROR HANDLING
 app.use(customErrorHandling);
+app.use(handle500);
 app.all("/*", handle404);
 
 module.exports = app;
--- errors/errors.js
+++ errors/errors.js
@@ -2,6 +2,7 @@ exports.handle404 = (err, req, res, next) => {
   if (err.statusCode === 404) {
     res.status(404).send("Page not found.");
   }
+  next(err);
 };
 
 exports.customErrorHandling = (err, req, res, next) => {
@@ -9,5 +10,11 @@ exports.customErrorHandling = (err, req, res, next) => {
     res.status(err.status).send({ msg: err.msg });
   } else {
     next(err);
+  }
+};
+
+exports.handle500 = (err, req, res, next) => {
+  if (err.statusCode === 500) {
+    res.status(500).send("Internal Server Error");
   }
 };
